e638ebe76def8b74532f156d2f291930e73bfccb
谢庆庆 <1546726199@qq.com>
2020-11-26 11:57:49 +0800

    update

---
+++ cvt-node/src/main/java/com/cvt/iri/service/dto/GetTipsResponse.java
@@ -0,0 +1,24 @@
+package com.cvt.iri.service.dto;
+
+import java.util.List;
+
+public class GetTipsResponse extends AbstractResponse {
+	
+	private String [] hashes;
+
+	public static AbstractResponse create(List<String> elements) {
+		GetTipsResponse res = new GetTipsResponse();
+		res.hashes = elements.toArray(new String[] {});
+		return res;
+	}
+	
+    /**
+     * The list of current tips
+     *
+     * @return The hashes.
+     */
+	public String[] getHashes() {
+		return hashes;
+	}
+
+}
