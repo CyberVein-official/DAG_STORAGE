f865b3f704d575d192e2078f79c6f87f7b953175
谢庆庆 <1546726199@qq.com>
2020-12-04 11:37:22 +0800

    'update'

---
+++ cvt-node/src/main/java/com/cvt/iri/service/dto/GetTransactionsToApproveResponse.java
@@ -0,0 +1,34 @@
+package com.cvt.iri.service.dto;
+
+import com.cvt.iri.model.Hash;
+
+public class GetTransactionsToApproveResponse extends AbstractResponse {
+
+    private String trunkTransaction;
+    private String branchTransaction;
+
+	public static AbstractResponse create(Hash trunkTransactionToApprove, Hash branchTransactionToApprove) {
+		GetTransactionsToApproveResponse res = new GetTransactionsToApproveResponse();
+		res.trunkTransaction = trunkTransactionToApprove.toString();
+		res.branchTransaction = branchTransactionToApprove.toString();
+		return res;
+	}
+	
+    /**
+     * The branch transaction
+     *
+     * @return The branch transaction.
+     */
+	public String getBranchTransaction() {
+		return branchTransaction;
+	}
+	
+    /**
+     * The trunk transaction
+     *
+     * @return The trunk transaction.
+     */
+	public String getTrunkTransaction() {
+		return trunkTransaction;
+	}
+}
