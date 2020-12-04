29a89e4c585c319a61e6483313912de33ef8cf66
zz <zz517141035@163.com>
2020-12-04 11:08:26 +0800

    UPDATE node fun

---
+++ cvt-node/src/main/java/com/cvt/iri/service/dto/RemoveNeighborsResponse.java
@@ -0,0 +1,22 @@
+package com.cvt.iri.service.dto;
+
+public class RemoveNeighborsResponse extends AbstractResponse {
+	
+	private int removedNeighbors;
+
+	public static AbstractResponse create(int numberOfRemovedNeighbors) {
+		RemoveNeighborsResponse res = new RemoveNeighborsResponse();
+		res.removedNeighbors = numberOfRemovedNeighbors;
+		return res;
+	}
+	
+    /**
+     * The number of removed neighbors.
+     *
+     * @return The number of removed neighbors.
+     */
+	public int getRemovedNeighbors() {
+		return removedNeighbors;
+	}
+
+}
