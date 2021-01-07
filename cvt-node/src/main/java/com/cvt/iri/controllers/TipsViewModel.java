19b6a43fd9cdc615d8b9cc9e7b92ae03dddc780d
AnJouYang <angelyang@cybervein.org>
2021-01-07 15:46:55 +0800

    upd node controllres

---
+++ cvt-node/src/main/java/com/cvt/iri/controllers/TipsViewModel.java
@@ -0,0 +1,43 @@
+package com.cvt.iri.controllers;
+
+import com.cvt.iri.model.Hash;
+
+import java.security.SecureRandom;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class TipsViewModel {
+
+    public static final int MAX_TIPS = 5000;
+
+    private final FifoHashCache<Hash> tips = new FifoHashCache<>(TipsViewModel.MAX_TIPS);
+    private final FifoHashCache<Hash> solidTips = new FifoHashCache<>(TipsViewModel.MAX_TIPS);
+
+    private final SecureRandom seed = new SecureRandom();
+    private final Object sync = new Object();
+
+    public void addTipHash(Hash hash) {
+        synchronized (sync) {
+            tips.add(hash);
+        }
+    }
+
+    public void removeTipHash(Hash hash) {
+        synchronized (sync) {
+            if (!tips.remove(hash)) {
+                solidTips.remove(hash);
+            }
+        }
+    }
+
+    public void setSolid(Hash tip) {
+        synchronized (sync) {
+            if (tips.remove(tip)) {
+                solidTips.add(tip);
+            }
+        }
+    }
+
+}
