4f47921db516620faf9bccff68b5d1179b55d407
谢庆庆 <1546726199@qq.com>
2021-01-11 14:23:05 +0800

    'addNodeModel'

---
+++ cvt-node/src/main/java/com/cvt/iri/model/Transaction.java
@@ -0,0 +1,56 @@
+package com.cvt.iri.model;
+
+import com.cvt.iri.controllers.TransactionViewModel;
+import com.cvt.iri.storage.Persistable;
+import com.cvt.iri.utils.Serializer;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by paul on 3/2/17 for iri.
+ */
+public class Transaction implements Persistable {
+    public static final int SIZE = 1604;
+
+    public byte[] bytes;
+
+    public Hash address;
+    public Hash bundle;
+    public Hash trunk;
+    public Hash branch;
+    public Hash obsoleteTag;
+    public long value;
+    public long currentIndex;
+    public long lastIndex;
+    public long timestamp;
+
+    public Hash tag;
+    public long attachmentTimestamp;
+    public long attachmentTimestampLowerBound;
+    public long attachmentTimestampUpperBound;
+
+    public int validity = 0;
+    public int type = TransactionViewModel.PREFILLED_SLOT;
+    public long arrivalTime = 0;
+
+    //public boolean confirmed = false;
+    public boolean parsed = false;
+    public boolean solid = false;
+    public long height = 0;
+    public String sender = "";
+    public int snapshot;
+
+    public byte[] bytes() {
+        return bytes;
+    }
+
+    public void read(byte[] bytes) {
+        if(bytes != null) {
+            this.bytes = new byte[SIZE];
+            System.arraycopy(bytes, 0, this.bytes, 0, SIZE);
+            this.type = TransactionViewModel.FILLED_SLOT;
+        }
+    }
+
+
+}
