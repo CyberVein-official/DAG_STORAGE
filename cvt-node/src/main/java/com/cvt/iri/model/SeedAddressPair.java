e64886c7896fca6f566b7e9a17f527f7b6f37a70
wangjiawei <1728641135@qq.com>
2021-02-07 11:07:23 +0800

    upd node models

---
+++ cvt-node/src/main/java/com/cvt/iri/model/SeedAddressPair.java
@@ -0,0 +1,16 @@
+package com.cvt.iri.model;
+
+import com.cvt.iri.storage.Indexable;
+
+/**
+ * Created by paul on 5/15/17.
+ */
+public class SeedAddressPair implements Indexable {
+
+    private String seed;
+    private String address;
+
+
+
+
+}
