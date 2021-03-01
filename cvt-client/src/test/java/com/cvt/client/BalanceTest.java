ecf831ed291e89c5574e38c1ca3947b7c7679e5a
wangjiawei <1728641135@qq.com>
2021-03-01 13:50:38 +0800

    upd test

---
+++ cvt-client/src/test/java/com/cvt/client/BalanceTest.java
@@ -0,0 +1,25 @@
+package com.cvt.client;
+
+import com.google.gson.Gson;
+import jota.dto.response.GetBalancesAndFormatResponse;
+import jota.dto.response.GetBalancesResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * 余额的用例
+ *
+ * @author cvt admin
+ * Time: 2018/11/10 : 11:26
+ */
+@Slf4j
+public class BalanceTest extends BaseTest {
+
+    @Test
+    public void get_balance() throws Exception {
+
+    }
+}
