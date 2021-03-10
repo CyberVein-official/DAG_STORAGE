96f53ff11c984abf2a674365a4395354071c1824
Stephen <952121091@qq.com>
2021-03-10 17:50:19 +0800

    update UdpClient

--- cvt-client/src/test/java/com/cvt/client/UDPClient.java
+++ cvt-client/src/test/java/com/cvt/client/UDPClient.java
@@ -34,5 +34,9 @@ public class UDPClient {
         DatagramPacket packet2 = new DatagramPacket(data2, data2.length);
         //2.接收服务器响应的数据
         socket.receive(packet2);
+
+        //3.读取数据
+        String reply = new String(data2, 0, packet2.getLength());
+        System.out.println("我是客户端，服务器说：" + reply);
     }
 }
