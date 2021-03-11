7a418dad167c6f3c7cf1aae30f632fdbb99ab908
zz <zz517141035@163.com>
2021-03-11 18:03:30 +0800

    UPDATE

---
+++ cvt-client/src/test/java/com/cvt/client/UDPServer.java
@@ -0,0 +1,22 @@
+package com.cvt.client;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * UDPServer
+ *
+ * @author cvt admin
+ * Time: 2018/11/26 : 15:07
+ */
+public class UDPServer {
+
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket(14672);
+
+        byte[] data = new byte[1024];
+
+
+    }
+}
