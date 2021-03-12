bb8c1ed456eaa8cf0ac5e81a7fa4e2658216c14b
wangjiawei <1728641135@qq.com>
2021-03-12 11:40:12 +0800

    upd test

--- cvt-client/src/test/java/com/cvt/client/UDPServer.java
+++ cvt-client/src/test/java/com/cvt/client/UDPServer.java
@@ -14,8 +14,10 @@ public class UDPServer {
 
     public static void main(String[] args) throws Exception {
         DatagramSocket socket = new DatagramSocket(14672);
-
         byte[] data = new byte[1024];
+
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+
 
 
     }
